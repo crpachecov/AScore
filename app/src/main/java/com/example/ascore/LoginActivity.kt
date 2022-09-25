1a2186b75d171887a521a223aa4bd705b0937b51
crpachecov <crpacheco@unicesar.edu.co>
2022-09-25 13:09:03 -0500

    Login sin funcionar

---
+++ app/src/main/java/com/example/ascore/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.ascore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+
+        setup()
+    }
+
+    private fun setup(){
+        title = "Login"
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnRegister.setOnClickListener {
+            if (etEmail.text.isNotEmpty() && etPassword.text.isNotEmpty()){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(etEmail.text.toString(),
+                    etPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        btnLogin.setOnClickListener {
+            if (etEmail.text.isNotEmpty() && etPassword.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(etEmail.text.toString(),
+                    etPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+}
