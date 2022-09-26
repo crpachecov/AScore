45fab32d43748fcff618cd9c80c4cb09062b47cd
crpachecov <crpacheco@unicesar.edu.co>
2022-09-25 22:37:16 -0500

    Registro de usuarios sin funcionar

---
+++ app/src/main/java/com/example/ascore/RegistroActivity.kt
@@ -0,0 +1,68 @@
+package com.example.ascore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+//            reload();
+        }
+    }
+
+    fun RegisterUser(view: View){
+
+        val email: EditText = findViewById(R.id.etEmailRegistro)
+        val password: EditText = findViewById(R.id.etPasswordRegistro)
+        val passwordConfirm: EditText = findViewById(R.id.etPasswordConfirm)
+
+
+        if (password.getText().toString().equals(passwordConfirm.getText().toString())){
+
+            auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "createUserWithEmail:success")
+                        Toast.makeText(applicationContext, "Usuario creado.",
+                            Toast.LENGTH_SHORT).show()
+//                        val user = auth.currentUser
+//                        updateUI(user)
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+                    }
+                }
+
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
