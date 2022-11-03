323c4f1cf35ae5dbe945485512661896e7bde042
crpachecov <crpacheco@unicesar.edu.co>
2022-11-03 10:41:31 -0500

    Views application in progress and Register User is finished

--- app/src/main/java/com/example/ascore/MainActivity.kt
+++ app/src/main/java/com/example/ascore/MainActivity.kt
@@ -4,17 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.ascore.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
         replaceFragment(Home())
 
         binding.bottomNavigationView.setOnItemSelectedListener {
@@ -31,6 +38,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+//            reload();
+        }
+    }
+
+    fun RegisterUser(view: View){
+
+        val email: EditText = findViewById(R.id.etEmailRegistro)
+        val password: EditText = findViewById(R.id.etPasswordRegistro)
+        val passwordConfirm: EditText = findViewById(R.id.etPasswordConfirm)
+
+
+        if (password.text.toString() == passwordConfirm.text.toString()){
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "createUserWithEmail:success")
+                        Toast.makeText(applicationContext, "Usuario creado.",
+                            Toast.LENGTH_SHORT).show()
+//                        val user = auth.currentUser
+//                        updateUI(user)
+                        replaceFragment(Login())
+                    } else {
+                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+                    }
+                }
+
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
     private fun replaceFragment(fragment : Fragment){
 
         val fragmentManager = supportFragmentManager
@@ -41,6 +91,10 @@ class MainActivity : AppCompatActivity() {
 
     fun irLogin(view: View){
         replaceFragment(Login())
+    }
+
+    fun irRegister(view: View){
+        replaceFragment(Register())
     }
 
     fun irProfile(view: View){
